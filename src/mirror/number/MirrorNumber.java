10855beedb1bec5b7e0bb1913cac151c12a6eb22
konda babu <konda babu@UNICORN77>
2021-06-12 00:11:28 +0530

    file

---
+++ src/mirror/number/MirrorNumber.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mirror.number;
+import java.util.Scanner;
+/**
+ *
+ * @author konda babu
+ */
+public class MirrorNumber {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int n=input.nextInt();
+        System.out.println(n);
+    }
+    
+}
